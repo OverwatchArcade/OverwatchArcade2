da60baf46d0880507de5daf93bfa5e4a4b96a583
Quintenps <quinten@peels.me>
2020-04-20 11:16:01 +0200

    [Feature] Manifest url update, manifest icon

--- resources/js/app.js
+++ resources/js/app.js
@@ -37,3 +37,10 @@ const app = new Vue({
     }
 });
 
+window.addEventListener("beforeinstallprompt", function(e) {
+    // log the platforms provided as options in an install prompt
+    console.log(e.platforms); // e.g., ["web", "android", "windows"]
+    e.userChoice.then(function(choiceResult) {
+        console.log(choiceResult.outcome); // either "accepted" or "dismissed"
+    }, handleError);
+});
